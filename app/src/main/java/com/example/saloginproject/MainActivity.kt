86c7e77b66dadfb34fb98478c674beed8f944ede
Admin <makansari@gmail.com>
2021-02-04 14:52:49 +0530

    registration screen

--- app/src/main/java/com/example/saloginproject/MainActivity.kt
+++ app/src/main/java/com/example/saloginproject/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.saloginproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -17,7 +18,7 @@ class MainActivity : AppCompatActivity() {
 
 
             var signFragment = SigninFragment()
-            supportFragmentManager.beginTransaction().add(R.id.mainConstraintLayout,signFragment).commit()
+            supportFragmentManager.beginTransaction().replace(R.id.mainConstraintLayout,signFragment).commit()
            buttonSubmit.visibility = View.GONE
 
         }
@@ -27,4 +28,9 @@ class MainActivity : AppCompatActivity() {
 
         }
     }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
 }
--- app/src/main/java/com/example/saloginproject/SigninFragment.kt
+++ app/src/main/java/com/example/saloginproject/SigninFragment.kt
@@ -1,10 +1,16 @@
 package com.example.saloginproject
 
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.singin_frag.*
+import kotlinx.android.synthetic.main.singin_frag.view.*
 
 class SigninFragment : Fragment() {
 
@@ -14,6 +20,32 @@ class SigninFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         var v = inflater.inflate(R.layout.singin_frag,container,false)
+       
+            
+       
+        v.buttonRegister.setOnClickListener {
+            var username = v.editTextsignUsername.text.toString()
+            var password = v.editTextsingPassword.text.toString()
+            var mobile = v.editTextsignMobile.text.toString()
+            
+            var alertDialog = AlertDialog.Builder(activity)
+                .setTitle("Alert Dialog")
+                .setMessage("The data is username is :$username \n password is : $password ")
+            alertDialog.setPositiveButton("OK",DialogInterface.OnClickListener { dialog, which ->
+                
+                Toast.makeText(activity,"Postive Button",Toast.LENGTH_LONG).show()
+            })
+            
+            alertDialog.setNegativeButton("Cancel",DialogInterface.OnClickListener { dialog, which ->
+
+                Toast.makeText(activity,"Negative Button",Toast.LENGTH_LONG).show()
+
+            })
+            var ad = alertDialog.create()
+            ad.show()
+           
+        }
+
         return v
     }
 }
